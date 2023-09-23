6a4c0e88ac7e2aecd4dbe5d18ef851a2c35cc6c6
david <david.lassig.ext@bwi.de>
2023-09-23 22:27:36 +0200

    updating Navigation_Solution and add checkpoints

--- dqn_agent.py
+++ dqn_agent.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
-from model import QNetwork
+from model import QNetworkDeeper as QNetwork
 
 import torch
 import torch.nn.functional as F
@@ -42,7 +42,13 @@ class Agent():
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
-    
+        
+    def save_checkpoint(self, cp_name="checkpoint.pth"):
+        torch.save(self.qnetwork_local.state_dict(), cp_name)
+        
+    def load_checkpoint(self, cp_name="checkpoint.pth"):
+        self.qnetwork_local.load_state_dict(torch.load(cp_name))
+
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
@@ -75,6 +81,35 @@ class Agent():
         else:
             return random.choice(np.arange(self.action_size))
 
+    def old_learn(self, experiences, gamma):
+        """Update value parameters using given batch of experience tuples.
+
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+        # Get max predicted Q values (for next states) from target model
+        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        # Compute Q targets for current states 
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+
+        # Get expected Q values from local model
+        print(actions)
+        Q_expected = self.qnetwork_local(states).gather(1, actions)
+
+        # Compute loss
+        loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # ------------------- update target network ------------------- #
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
+        
     def learn(self, experiences, gamma):
         """Update value parameters using given batch of experience tuples.
 
@@ -101,7 +136,7 @@ class Agent():
         self.optimizer.step()
 
         # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)                     
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU) 
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
@@ -26,3 +27,64 @@ class QNetwork(nn.Module):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
+
+    
+class QNetworkDeeper(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetworkDeeper, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, 256)
+        self.fc4 = nn.Linear(256, 128)
+        self.fc5 = nn.Linear(128, 64)
+        self.fc6 = nn.Linear(64, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = self.relu(self.fc1(state))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.relu(self.fc4(x))
+        x = self.relu(self.fc5(x))
+        Q = self.fc6(x)
+        
+        return Q
+    
+    
+class QNetworkDueling(nn.Module):
+    def __init__(self, state_size, action_size, seed):
+        
+        super(QNetworkDueling, self).__init__()
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, 256)
+        self.fc_adv = nn.Linear(64, 256)
+
+        self.value = nn.Linear(256, 1)
+        self.adv = nn.Linear(256, 2)
+
+    def forward(self, state):
+        y = self.relu(self.fc1(state))
+        value = self.relu(self.fc2(y))
+        adv = self.relu(self.fc_adv(y))
+
+        value = self.value(value)
+        adv = self.adv(adv)
+
+        advAverage = torch.mean(adv, dim=1, keepdim=True)
+        Q = value + adv - advAverage
+
+        return Q
