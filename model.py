918210546d36b810b74aced1ab93529ee6d45a48
david <david.lassig.ext@bwi.de>
2023-09-26 15:28:41 +0200

    add files

--- model.py
+++ model.py
@@ -6,26 +6,25 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+    def __init__(self, state_size, action_size, seed):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.relu = nn.ReLU()
         self.fc1 = nn.Linear(state_size, 128)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.fc3 = nn.Linear(64, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
+        x = self.relu(self.fc1(state))
+        x = self.relu(self.fc2(x))
         return self.fc3(x)
 
     
@@ -39,8 +38,6 @@ class QNetworkDeeper(nn.Module):
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
         """
         super(QNetworkDeeper, self).__init__()
         self.seed = torch.manual_seed(seed)
@@ -63,28 +60,3 @@ class QNetworkDeeper(nn.Module):
         
         return Q
     
-    
-class QNetworkDueling(nn.Module):
-    def __init__(self, state_size, action_size, seed):
-        
-        super(QNetworkDueling, self).__init__()
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(state_size, 64)
-        self.fc2 = nn.Linear(64, 256)
-        self.fc_adv = nn.Linear(64, 256)
-
-        self.value = nn.Linear(256, 1)
-        self.adv = nn.Linear(256, 2)
-
-    def forward(self, state):
-        y = self.relu(self.fc1(state))
-        value = self.relu(self.fc2(y))
-        adv = self.relu(self.fc_adv(y))
-
-        value = self.value(value)
-        adv = self.adv(adv)
-
-        advAverage = torch.mean(adv, dim=1, keepdim=True)
-        Q = value + adv - advAverage
-
-        return Q
